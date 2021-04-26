3eee3709db606b4e89e8a5addc206d0f7866b383
nkpn <illinmikita@gmail.com>
2021-04-26 17:58:05 +0300

    Done!

--- js/task-08.js
+++ js/task-08.js
@@ -4,22 +4,32 @@ const buttonCreate = document.getElementsByTagName('button')[0];
 const buttonDelete = document.getElementsByTagName('button')[1];
 const divArr = document.getElementById('boxes');
 
+
+
+const randomColor = (min, max) =>
+    Math.round(Math.random() * (max - min) + min);
+
+
 const createDiv = (event) => {
     let num = Number(input.value);
+    const ArrayEl = [];
     
     for (let i = 0; i <= num; i++){
-        console.log('is it works?')
         const divEl = document.createElement('div');
-        divEl.style.width = 30;
-        divEl.style.height = 30;
-        return divEl;
+        divEl.style.backgroundColor = `RGB(
+            ${randomColor(0, 255)},
+            ${randomColor(0, 255)},
+            ${randomColor(0, 255)})`;
+        divEl.style.width = `${30 + i * 10}px`;
+        divEl.style.height = `${30 + i * 10}px`;
+        ArrayEl.push(divEl);
     }
-
-    divArr.appendChild(divEl);
+    divArr.append(...ArrayEl);
 }
 
 const deleteDiv = (event) => {
-    
+    input.value = '';
+    divArr.innerHTML = '';
 }
 
 buttonCreate.addEventListener('click', createDiv);
